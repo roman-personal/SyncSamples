ffe5d3c6e0419a6fc44d3c979c2d1754823816c7
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2022-01-31 12:22:37 +0300

    Monitor sample

---
+++ MonitorSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonitorSample {
+    // Пример использования Monitor для предотвращения состояния гонки
+    internal class Program {
+        static int value = 0;
+        static object sync = new object();
+
+        static void Main(string[] args) {
+            var tasks = new Task[] { Task.Run(Method1), Task.Run(Method2) };
+            Task.WaitAll(tasks);
+            Console.WriteLine("Done!");
+        }
+
+        static void Method1() {
+            int count = 100;
+            while (count-- > 0) {
+                Monitor.Enter(sync);
+                value++;
+                Monitor.Exit(sync);
+                Thread.Sleep(3);
+            }
+        }
+
+        static void Method2() {
+            int count = 100;
+            while (count-- > 0) {
+                Monitor.Enter(sync);
+                if (value % 2 == 0)
+                    Console.WriteLine(value);
+                Monitor.Exit(sync);
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
