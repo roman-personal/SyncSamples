46546603fd3b98d914df0b17ae968e75818f3923
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2022-01-31 15:24:41 +0300

    Interlocked sample

---
+++ InterlockedSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterlockedSample {
+    internal class Program {
+        static int value = 0;
+
+        static void Main(string[] args) {
+            var tasks = new Task[] { Task.Run(DoIncrement), Task.Run(DoIncrement) };
+            Task.WaitAll(tasks);
+            Console.WriteLine($"Done! Value: {value}");
+        }
+
+        static void DoIncrement() {
+            int count = 5000;
+            while (count-- > 0)
+                Interlocked.Increment(ref value);
+        }
+    }
+}
